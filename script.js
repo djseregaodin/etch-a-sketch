46edd3aa48f52edb67792d4b8e156f12e7fce5e9
djseregaodin <251333467+djseregaodin@users.noreply.github.com>
2026-02-17 12:03:34 +0300

    Add square grid with hover effect

---
+++ script.js
@@ -0,0 +1,15 @@
+const main = document.querySelector("main")
+
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement("div");
+    row.setAttribute("class", "row");
+    for (let j = 0; j < 16; j++) {
+        const div = document.createElement("div");
+        div.setAttribute("class", "square");
+        div.addEventListener("mouseenter", () => {
+            div.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)} ${Math.floor(Math.random() * 255)} ${Math.floor(Math.random() * 255)})`;
+        })
+        row.appendChild(div);
+    }
+    main.appendChild(row);
+}
