de0d0588ded1f3665f27a18d9edf68c74761b71c
djseregaodin <251333467+djseregaodin@users.noreply.github.com>
2026-02-17 12:36:41 +0300

    Add a grid reset button and refactor grid creation

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
-const grid = document.querySelector("#grid")
+// Square grid constructor
+const createGrid = (parentElement, dimensions) => {
+    for (let i = 0; i < dimensions; i++) {
+        const row = document.createElement("div");
+        row.setAttribute("class", "row");
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    row.setAttribute("class", "row");
-    for (let j = 0; j < 16; j++) {
-        const div = document.createElement("div");
-        div.setAttribute("class", "square");
-        div.addEventListener("mouseenter", () => {
-            div.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)} ${Math.floor(Math.random() * 255)} ${Math.floor(Math.random() * 255)})`;
-        })
-        row.appendChild(div);
+        for (let j = 0; j < dimensions; j++) {
+            const div = document.createElement("div");
+            div.setAttribute("class", "square");
+            div.addEventListener("mouseenter", () => {
+                div.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)} ${Math.floor(Math.random() * 255)} ${Math.floor(Math.random() * 255)})`;
+            })
+
+            row.appendChild(div);
+        }
+
+        parentElement.appendChild(row);
     }
-    grid.appendChild(row);
 }
+
+const grid = document.querySelector("#grid");
+
+createGrid(grid, 16);
+
+const gridReset = document.querySelector("#grid-reset");
+
+gridReset.addEventListener("click", () => {
+    const dimensions = Math.floor(Number(prompt("Enter the number of squares per side for the new grid:", "16")));
+    if (dimensions > 100) {
+        dimensions = 100;
+    }
+
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+
+    createGrid(grid, dimensions);
+})
